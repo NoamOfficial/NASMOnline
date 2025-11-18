c269dd478f742bff61699a0eae93e9600102e36f
NoamOfficial <noamrosenblit@gmail.com>
2025-11-18 17:38:25 +0200

    Create ShellResources.asm

---
+++ ShellResources.asm
@@ -0,0 +1,17 @@
+Desktop:
+Times 1680 dd 0xDBAAAA
+StartIcon:
+line1 db 0xDBAA, 0xDBAA, 0xDBAA, 0xDBAA, 0xDBAA, 0xDBAA
+Line2 dd 0xDBAA, 0xDBAA, 0xDBFF, 0xDBFFFF55, 0xDBFFFF55, 0xDBFFFF55
+line3 dd 0xDBAA, 0xDBAA, 0xDBFF, 0xDBFFFF55, 0xDB55FF55, 0xDB55FF55
+Line4 dd 0xDBAA, 0xDBAA, 0xDBFFFF55, 0xDBAA0000, 0xDB55FF55, 0xDBFFFF55
+Dialog:
+DialogName resd 8
+DialogBorderTop times 32 db 0x00
+DialogLine1 db 0xDB00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00
+DialogLine2 db 0xDB00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00
+DialogLine3 db 0xDB00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00
+DialogLine4 db 0xDB00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00
+DialogBorderBottom times 32 db 0x00
+
+
