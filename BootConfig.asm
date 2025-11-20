5b7b3bb4c4bb62087b495c3b1b0ffd7f0973d5b2
NoamOfficial <noamrosenblit@gmail.com>
2025-11-20 19:23:04 +0200

    Update BootConfig.asm

--- BootConfig.asm
+++ BootConfig.asm
@@ -3,10 +3,10 @@ global useatadrivers
 global OSVersion
 global DataWipe
 Config:
-testsigning db 0
-useatadrivers db 0
+testsigning db 2
+useatadrivers db 2
 OSVersion dd 0x10
-DataWipe db 0
+DataWipe db 2
 DiskDriverConfig:
 diskMode dw "NATA"
 ControllerInLegacyMode db 0
@@ -16,7 +16,7 @@ Global diskMode
 global ControllerInLegacyMode
 global ISDEnabled
 Entry0:
-BootSectorStartingLBA dw 0
+BootSectorStartingLBA dw 2
 BootSectorCount dw 4
 Entry1: resb 4
 Entry2: resb 4
