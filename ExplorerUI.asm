f770c07003ede82a08f1f1fcd9118d48bdcf3763
NoamOfficial <noamrosenblit@gmail.com>
2025-11-21 06:50:14 +0200

    Update ExplorerUI.asm

--- ExplorerUI.asm
+++ ExplorerUI.asm
@@ -2,8 +2,7 @@ NavigationBar:
 times 50 dd 0xAAAAAA
 SystemFile:
 times 5 db 0xDB00, 0xDB00, 0xDBAA, 0xDBAA, 0xDBAA
-AddressBar
+AddressBar:
 Bar db 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x00
-CADSequenceTerminate:
-Sequence db 0F, 0A, 0B, 00, 03
+
 
