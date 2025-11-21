9ba0b1c69d3f778a1c57456b22626bba074af7c9
NoamOfficial <noamrosenblit@gmail.com>
2025-11-21 13:06:15 +0200

    Update PCP.asm

--- PCP.asm
+++ PCP.asm
@@ -2,22 +2,40 @@ cmp ah, 0
 je PCVMRun
 cmp ah, 1
 je PCVMTerminate
+jmp PCVMDone            ; if unknown
+
+; -------------------------
+; Run 16-bit mode procedure
+; -------------------------
 PCVMRun:
-push ip
-push cs
-push ds
-push EFLAGS
-[BITS 16]
-call edx
-[BITS 32]
-IRET
+    pushf                ; save flags
+    push ds              ; save ds
+    push cs              ; save cs
+    push ip              ; save ip (NASM replaces it properly)
+
+    [BITS 16]
+    call edx             ; run 16-bit code
+    [BITS 32]
+
+    ; restore state
+    pop ip
+    pop cs
+    pop ds
+    popf
+
+    iret
+
+; -------------------------
+; Terminate VM
+; -------------------------
 PCVMTerminate:
-pop eax
-pop ebx
-pop ecx
-pop edx
-mov ecx, 0
-mov eax, 0
-mov ebx, 0
-mov edx, 0
-IRET
+    ; zero registers (optional)
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+    xor edx, edx
+    iret
+
+PCVMDone:
+    iret
+
