2d41cdc93bccb0bd9dcd9b8e1ac6b40cdb81eba3
NoamOfficial <noamrosenblit@gmail.com>
2025-12-06 19:30:16 +0200

    Update fileops.asm

--- fileops.asm
+++ fileops.asm
@@ -1,18 +1,2 @@
-section .data
-MFTBase dd 0x02
-FileStart resd 8
-section .text
-WriteFile:
-MOV AH, 0
-MOV DX, FileStart
-call [SET_SECTOR_NUM_32]
-int 13h
-ret
-ReadFile:
-MOV AH, 1
-MOV DX, FileStart
-int 13h
-ret
-global ReadFile
-global WriteFile
+
 
